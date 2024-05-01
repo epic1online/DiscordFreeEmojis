18e6ff9079e0f39c9621decb281f0e00b766715f
epic1online <43654580+epic1online@users.noreply.github.com>
2024-05-01 14:28:03 -0400

    Change to using cdn link directly
    emoji.url no longer exists

--- DiscordFreeEmojis64px.user.js
+++ DiscordFreeEmojis64px.user.js
@@ -114,7 +114,7 @@ function Init(final)
     }
 
     function replaceEmoji(parseResult, emoji) {
-        parseResult.content = parseResult.content.replace(`<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`, emoji.url.split("?")[0] + "?size=64");
+        parseResult.content = parseResult.content.replace(`<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`, `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.animated ? 'gif' : 'webp'}?size=64`);
     }
 	
     const original_parse = messageEmojiParserModule.parse;
